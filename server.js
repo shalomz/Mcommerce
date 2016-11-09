2918fa9326d95b7630a49cde82c6b14c7fe1d4ed
cmutwol <cyrusmutwol@gmail.com>
2016-11-09 09:20:41 +0300

    mpesa

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = http.createServer(function (req, res) {
 });
 
 function displayForm(res) {
-    fs.readFile('form.html', function (err, data) {
+    fs.readFile('views/form.html', function (err, data) {
         res.writeHead(200, {
             'Content-Type': 'text/html',
                 'Content-Length': data.length
